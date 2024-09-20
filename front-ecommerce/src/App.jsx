15ae732eefda8323ebfeecb2228db8b11cdaaea8
Priscila Di Renzo <prisciladirenzo@gmail.com>
2024-09-20 19:12:22 -0300

    initial routing

--- front-ecommerce/src/App.jsx
+++ front-ecommerce/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Landing from './components/landing/Landing';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />,
+    },
+  ]);
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div>
+        <RouterProvider router={router} />
+     </div>
     </>
   )
 }
 
-export default App
+export default App;
